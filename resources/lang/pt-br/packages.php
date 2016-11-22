bb65afa34bf3e341bb543ae47df024f3e7215ac8
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-22 17:40:23 +0200

    Applied fixes from StyleCI (#3)
    [ci skip] [skip ci]

--- resources/lang/pt-br/packages.php
+++ resources/lang/pt-br/packages.php
@@ -2,5 +2,5 @@
 
 return [
     'faustbrian_laravel_validation_required' => "Componente requerido. Por favor instale a dependência usando o comando: 'composer require faustbrian/laravel-validation'",
-    'league_fractal_required'             => "Componente requerido. Por favor instale a dependência usando o comando:  'composer require league/fractal' (0.12.*)",
+    'league_fractal_required'                => "Componente requerido. Por favor instale a dependência usando o comando:  'composer require league/fractal' (0.12.*)",
 ];
--- resources/lang/ro/packages.php
+++ resources/lang/ro/packages.php
@@ -2,5 +2,5 @@
 
 return [
     'faustbrian_laravel_validation_required' => "Pachet obligatoriu. Instalaţi faustbrian/laravel-validation ('composer require faustbrian/laravel-validation'), vă rog.",
-    'league_fractal_required'             => "Pachet obligatoriu. Instalaţi league/fractal ('composer require league/fractal') (0.12.*), vă rog.",
+    'league_fractal_required'                => "Pachet obligatoriu. Instalaţi league/fractal ('composer require league/fractal') (0.12.*), vă rog.",
 ];
