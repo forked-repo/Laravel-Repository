c083941c2216e4de16a067c211e5bd21797a98e7
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 08:48:44 +0200

    Applied fixes from StyleCI (#5)
    [ci skip] [skip ci]

--- src/Repository/Contracts/RepositoryCriteriaInterface.php
+++ src/Repository/Contracts/RepositoryCriteriaInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Repository.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Repository\Contracts;
 
 use Illuminate\Support\Collection;
